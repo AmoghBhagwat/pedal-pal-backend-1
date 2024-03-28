6830ab244f64c6f159af16451024a45e1ad965e4
AmoghBhagwat <bhagwata22@iitk.ac.in>
2024-03-28 21:38:55 +0530

    fix typo

--- pedalpal/payment/tests.py
+++ pedalpal/payment/tests.py
@@ -8,35 +8,40 @@ class PaymentTestCase(APITestCase):
     def test_add_payment(self):
         self.client.force_authenticate(user=self.user)
         data = {
-            "amount":2000, 
+            "amount": 2000,
         }
         response = self.client.post("/payment/add_payment/", data)
         print(response.json())
         self.assertEqual(response.status_code, 200)
-    
+
     def test_update_balance(self):
         self.client.force_authenticate(user=self.user)
         data = {
-            "amount":1000, 
+            "amount": 1000,
         }
         response = self.client.post("/payment/update_balance/", data)
         print(response.json())
         self.assertEqual(response.status_code, 200)
-        
+
+        data["amount"] = -1000
+        response = self.client.post("/payment/update_balance/", data)
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
+
     def test_get_balance(self):
         self.client.force_authenticate(user=self.user)
         data = {}
         response = self.client.get("/payment/get_balance/", data)
         print(response.json())
         self.assertEqual(response.status_code, 200)
-        
+
     def test_get_transaction(self):
         self.client.force_authenticate(user=self.user)
         data = {}
         response = self.client.get("/payment/get_transactions/", data)
         print(response.json())
-        self.assertEqual(response.status_code, 200)      
-        
+        self.assertEqual(response.status_code, 200)
+
     def setUp(self):
         self.user = Profile.objects.create(
             email="test@user.com",
@@ -45,4 +50,3 @@ class PaymentTestCase(APITestCase):
             phone="1234567890",
             password="test1234",
         )
-        
